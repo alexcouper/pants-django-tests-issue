5aff067c59939bf9d5618ba023816d8aaab27c89
Alex Couper <alex@jikoservices.com>
2018-10-01 09:32:21 +0000

    @jsirois suggested approach (https://github.com/pantsbuild/pants/issues/6526#issuecomment-425187962)

---
+++ src/python/project2/conftest.py
@@ -0,0 +1,21 @@
+import os
+
+
+_CLEANUPS = {}
+
+
+def _cleanup(name, value):
+    if value is None:
+        del os.environ[name]
+    else:
+        os.environ[name] = value
+
+
+def pytest_runtest_setup(item):
+    old_value = os.environ.get('DJANGO_SETTINGS_MODULE')
+    _CLEANUPS[item] = lambda: _cleanup('DJANGO_SETTINGS_MODULE', old_value)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'project2.config.settings'
+
+
+def pytest_runtest_teardown(item, nextitem):
+    _CLEANUPS.get(item, lambda: None)()
